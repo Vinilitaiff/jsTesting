aa9049e95c19c3a633e6380cd09696b30d3d114f
vinilitaiff <vinilitaiff@gmail.com>
2022-03-26 23:15:55 -0300

    trabalhando com studs

---
+++ 02-stubs/src/service.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+class Service {
+  async makeRequest(url) {
+
+    return new Promise(function (resolve, reject) {
+      https.get(url, response => {
+        response.on("data", data => resolve(JSON.parse(data)));
+        response.on("error", reject);
+      })
+    })
+  }
+
+  async getPlanets(url) {
+    const result = await this.makeRequest(url)
+
+    return {
+      name: result.name,
+      sufaceWater: result.surface_water,
+      appearedIn: result.films.length
+    }
+  }
+}
+
+//swapi.dev/api/planets/1/'
+
+module.exports = Service
---
+++ 02-stubs/src/service.test.js
@@ -0,0 +1,43 @@
+const Service = require('./service');
+const { deepStrictEqual } = require('assert')
+const sinon = require('sinon');
+const BASE_URl_1 = 'https://swapi.dev/api/planets/1/'
+const BASE_URl_2 = 'https://swapi.dev/api/planets/2/'
+const mocks = {
+  tatooine: require('./mocks/tatooine.json'),
+  alderaan: require('./mocks/alderaan.json'),
+}
+  ; (async () => {
+
+    const service = new Service();
+    const stub = sinon.stub(service, service.makeRequest.name);
+
+    stub
+      .withArgs(BASE_URl_1)
+      .resolves(mocks.tatooine);
+
+    stub
+      .withArgs(BASE_URl_2)
+      .resolves(mocks.alderaan);
+
+    {
+      const expected = {
+        'name': 'Tatooine',
+        'sufaceWater': '1',
+        appearedIn: 5
+      }
+      const results = await service.getPlanets(BASE_URl_1);
+      deepStrictEqual(results, expected);
+    }
+
+    {
+      const expected = {
+        'name': 'Alderaan',
+        'sufaceWater': '40',
+        appearedIn: 2
+      }
+
+      const results = await service.getPlanets(BASE_URl_2);
+      deepStrictEqual(results, expected);
+    }
+  })()
